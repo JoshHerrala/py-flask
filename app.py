598a0b2c315fd0f345498e8284b1547dd5d64671
JoshHerrala <josh@unionbayproject.com>
2021-10-21 11:43:59 -0400

    Add conditional to template

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask import render_template
 
+#TODO, refactor to seperate routing file
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def index():
 	user = {'username': 'Lucas'}
-	return render_template('index.html')
+	return render_template('index.html', title='Global Megacorp Home', user=user)
 
 @app.route("/greeting/")
 def greeting():
