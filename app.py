52966442684996c92a232298567318bc177ed39b
JoshHerrala <josh@unionbayproject.com>
2021-10-21 11:01:45 -0400

    add template directory

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
 def index():
-	return "Hello Wolford"
+	user = {'username': 'Lucas'}
+	return render_template('index.html')
 
 @app.route("/greeting/")
 def greeting():
