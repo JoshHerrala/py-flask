2feb2a4af9e60714f617ec722fc7f69ca46d850f
JoshHerrala <josh@unionbayproject.com>
2021-10-21 21:53:01 -0400

    login page

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from config import Config
 
 #TODO, refactor to seperate routing file
 app = Flask(__name__)
+app.config.from_object(Config)
 user_name = "test value"
 import routes
--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from forms import LoginForm
 from app import user_name
 
 
@@ -18,6 +19,11 @@ def index():
         }
     ]
 	return render_template('index.html', title='Global Megacorp Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 
 @app.route("/greeting/")
 def greeting():
