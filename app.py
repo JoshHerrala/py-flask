3a2dbbac0ff6bf2c605b5b7ae39965598cd4ede9
JoshHerrala <josh@unionbayproject.com>
2021-11-03 14:09:27 -0400

    add mongodb connnection

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
+import pymongo
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 user_name = "test value"
+client = pymongo.MongoClient("mongodb+srv://mongo_base_user_1:<PZZWERD>@cluster0.0zbkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.test
+#breakpoint()
 import routes
