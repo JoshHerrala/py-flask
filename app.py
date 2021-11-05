2f70eb8097c6c9eb6519f82ba61b4ab737b3e5e4
JoshHerrala <josh@unionbayproject.com>
2021-11-05 15:05:54 -0400

    clean up

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ app.config.from_object(Config)
 
 user_name = "test value"
 def db_connection():
-	mongo_client = pymongo.MongoClient("mongodb+srv://mongo_base_user_1:<pzz>@cluster0.0zbkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+	mongo_client = pymongo.MongoClient("mongodb+srv://mongo_base_user_1:<P>@cluster0.0zbkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
 	mg_db = mongo_client["maindb"]
+	#breakpoint()
 	return mg_db
 
 def load_sample_data():
-	client = db_connection()
+	db = db_connection()
 	#breakpoint()
-	emp_collection = client["employees"]
-	client.employees.remove({})
+	emp_collection = db["employees"]
+	db.employees.remove({})
 	emp1 = { "first_name": "Lucas", "primary_address": "123 Main Street"}
 	emp_collection.insert_one(emp1)
 	first_record = emp_collection.find_one()
@@ -28,4 +29,4 @@ import routes
 #breakpoint()
 #find all
 #array = list(emp_collection.find())
-#client.list_collection_names()
+#db.list_collection_names()
