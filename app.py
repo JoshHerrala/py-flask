8f712a8c3908eae587d630d5480b3c94d8aa3943
JoshHerrala <josh@unionbayproject.com>
2021-11-05 12:26:13 -0400

    create db connection method

--- app.py
+++ app.py
@@ -4,19 +4,26 @@ import pymongo
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 user_name = "test value"
-mongo_client = pymongo.MongoClient("mongodb+srv://mongo_base_user_1:<pzz>@cluster0.0zbkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+def db_connection():
+	mongo_client = pymongo.MongoClient("mongodb+srv://mongo_base_user_1:<pzz>@cluster0.0zbkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+	mg_db = mongo_client["maindb"]
+	return mg_db
 
-mg_db = mongo_client["maindb"]
+def load_sample_data():
+	client = db_connection()
+	#breakpoint()
+	emp_collection = client["employees"]
+    #emp1 = { "first_name": "Lucas", "primary_address": "123 Main Street"}
+	#x = emp_collection.insert_one(emp1)
+	first_record = emp_collection.find_one()
+	#breakpoint()
+	return first_record
 
-emp_collection = mg_db["employees"]
-
-emp1 = { "first_name": "Lucas", "primary_address": "123 Main Street"}
-#breakpoint()
-#x = emp_collection.insert_one(emp1)
-first_record = emp_collection.find_one()
-
-#find all below
-#array = list(emp_collection.find(myquery))
-#breakpoint()
+first_record = load_sample_data()
 import routes
+
+#breakpoint()
+#find all
+#array = list(emp_collection.find(myquery))
