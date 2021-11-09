13048eb2f5b00c1a91f0d2d8dd7a45111d54a4ff
JoshHerrala <josh@unionbayproject.com>
2021-11-08 22:06:03 -0500

    add employee create

--- app.py
+++ app.py
@@ -7,16 +7,23 @@ app.config.from_object(Config)
 
 user_name = "test value"
 def db_connection():
-	mongo_client = pymongo.MongoClient("mongodb+srv://mongo_base_user_1:<Pzzz>@cluster0.0zbkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+	mongo_client = pymongo.MongoClient("mongodb+srv://mongo_base_user_1:<pzz>@cluster0.0zbkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
 	mg_db = mongo_client["maindb"]
 	#breakpoint()
 	return mg_db
 
-def load_sample_data():
+def employees_collection():
 	db = db_connection()
 	#breakpoint()
 	emp_collection = db["employees"]
-	db.employees.remove({})
+	return emp_collection
+
+def load_sample_data():
+	emp_collection = employees_collection()
+	# db = db_connection()
+	#breakpoint()
+	# emp_collection = db["employees"]
+	emp_collection.remove({})
 	lead_dev = { "first_name": "Lucas", "primary_address": "123 Main Street"}
 	dev_team = [
       {"first_name": "Mia", "primary_address": "456 Harvey Milk Street"},
@@ -30,6 +37,9 @@ def load_sample_data():
 	return first_record, all_records
 
 first_record = load_sample_data()
+emp = employees_collection()
+
+
 
 #breakpoint()
 import routes
--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request
 from app import app
 from forms import LoginForm
-from app import first_record
+from app import first_record, emp
 
 @app.route("/")
 @app.route("/index")
@@ -28,6 +28,22 @@ def login():
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
 
-@app.route("/greeting/")
-def greeting():
-	return "Nice to see you"
+@app.route('/employees', methods=['GET', 'POST'])
+def create():
+	if request.method == 'GET':
+		return render_template('create_employee.html')
+ 
+	if request.method == 'POST':
+		#breakpoint()
+		#employee_id = request.form['employee_id']
+		first_name = request.form['first_name']
+		primary_address = request.form['primary_address']
+		#position = request.form['position']
+		# employee = EmployeeModel(employee_id=employee_id, name=name, age=age, position = position)
+		# db.session.add(employee)
+		# db.session.commit()
+		new_employee = { "first_name": first_record, "primary_address": primary_address}
+		#breakpoint()
+		#TODO import collection from app
+		emp.insert_one(new_employee)
+		return redirect('/index')
