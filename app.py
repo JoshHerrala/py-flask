c62885c52c0bfcb547486ec78b44a0712691e3bb
JoshHerrala <josh@unionbayproject.com>
2021-11-09 11:31:56 -0500

    add models

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from config import Config
 import pymongo
+import models
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 user_name = "test value"
 def db_connection():
-	mongo_client = pymongo.MongoClient("mongodb+srv://mongo_base_user_1:<paz>@cluster0.0zbkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+	mongo_client = pymongo.MongoClient("mongodb+srv://mongo_base_user_1:<>@cluster0.0zbkf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
 	mg_db = mongo_client["maindb"]
 	#breakpoint()
 	return mg_db
@@ -35,7 +36,7 @@ def load_sample_data():
 
 first_record = load_sample_data()
 emp = employees_collection()
-
+user_1 = models.Employee("bob","123 main","dev")
 
 
 #breakpoint()
---
+++ models.py
@@ -0,0 +1,6 @@
+class Employee:
+	def __init__(self, first_name, primary_address, position):
+		self.first_name = first_name
+		self.primary_address = primary_address
+		self.position = position
+	moto = "Alway move forward"
